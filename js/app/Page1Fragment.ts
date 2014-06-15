91368f9640f65abc6bc16f7dfd6634781a054bb4
naosim <nao_pillows@yahoo.co.jp>
2014-06-15 20:39:39 +0900

    現在のページだけ表示するようにした

--- js/app/Page1Fragment.ts
+++ js/app/Page1Fragment.ts
@@ -1,4 +1,4 @@
 /// <reference path="../makimaki/Fragment.ts" />
 class Page1Fragment extends Fragment {
-    getHtml(): string { return 'page1'; }
+    getHtml(): string { return 'page1 <a href="#/page2">jump to page2</a>'; }
 }
--- js/makimaki/RouterFragment.ts
+++ js/makimaki/RouterFragment.ts
@@ -12,31 +12,86 @@ class FragmentId {
     }
 }
 
+class FragmentHistory {
+    history: {id: FragmentId; fragment: Fragment}[];
+    constructor(){ this.history = [] }
+    current(): {id: FragmentId; fragment: Fragment} {
+        return this.history.length > 0 ? this.history[this.history.length - 1] : null;
+    }
+    push(e: {id: FragmentId; fragment: Fragment}) {
+        this.history.push(e);
+    }
+    pop(): {id: FragmentId; fragment: Fragment} {
+        return this.history.pop();
+    }
+}
+
+class FragmentsController {
+    fragmentHistory = new FragmentHistory();
+    stopLastFragment() {
+        var current = this.fragmentHistory.current();
+        if(!current) return;
+        
+        current.fragment.onStop();
+        $(current.id.cssId).css('display', 'none');
+    }
+
+    pop() {
+        this.stopLastFragment();
+        
+        var current = this.fragmentHistory.current();
+        var fragment = current.fragment;
+        fragment.destroy();
+        $(current.id.cssId).remove();
+        this.fragmentHistory.pop();
+        
+        $(this.fragmentHistory.current().id.cssId).css('display', 'block');
+        this.fragmentHistory.current().fragment.onStart();
+    }
+
+    push(fragmentClass: typeof Fragment) {
+        this.stopLastFragment();
+        
+        var id = FragmentId.create();
+        var html = '<li id="' + id.htmlId + '"></li>';
+        $(RouterFragment.routerId.cssId).append(html);
+
+        var parent = this.fragmentHistory.current() ? this.fragmentHistory.current().fragment : null;
+        var current = new fragmentClass();
+        this.fragmentHistory.push({id: id, fragment: current});
+        $(id.cssId).css('display', 'block');
+        current.init($(id.cssId), parent);
+        current.onStart();
+    }
+
+    current(): Fragment {
+        var current = this.fragmentHistory.current();
+        return current ? current.fragment : null;
+    }
+}
+
 class RouterFragment extends Fragment {
-    current: Fragment;
-    currentId: FragmentId;
-    router: Router;
+    router = new Router();
+    fragmentsController = new FragmentsController();
     static routerId = FragmentId.create('router_list');
     constructor(){
         super();
-        this.router = new Router();
     }
 
-    init($container: JQuery, parent?: Fragment) {
-        super.init($container, parent);
-    }
+    init($container: JQuery, parent?: Fragment) { super.init($container, parent); }
+
     getHtml(): string { return '<ul id="' + RouterFragment.routerId.htmlId + '"></ul>'; }
+    
     setManifest(ary: {url:string; fragmentClass:typeof Fragment; isFirst?: boolean}[]){
         var firstLocation;
                 
         var action = (manifest: {url:string; fragmentClass:typeof Fragment; isFirst?: boolean}) => {            
-            var isBack = () => {
-                return this.current && this.current.parent instanceof manifest.fragmentClass;
-            };
-            
             var routingAction = (req:any, next: any)=>{
-                if(this.current) this.current.onStop();
-                isBack() ? this.showBackPage() : this.showNewPage(manifest.fragmentClass);
+                var isBack = () => {
+                    var fragment = this.fragmentsController.current();
+                    return fragment && fragment.parent instanceof manifest.fragmentClass;
+                };
+                isBack() ? this.fragmentsController.pop() : this.fragmentsController.push(manifest.fragmentClass);
             };
             
             this.router.addRoute(manifest.url, routingAction);
@@ -51,27 +106,5 @@ class RouterFragment extends Fragment {
             // 直接きた ex) index.html#page2
             this.router._onHashChange();
         }
-    }
-    
-    showBackPage() {
-        console.log("back: " + this.currentId);
-        this.current.destroy();
-        $(this.currentId.cssId).remove();
-        this.current = this.current.parent;
-        this.current.onStart();
-    }
-
-    showNewPage(fragmentClass: typeof Fragment) {
-        // new
-        var id = FragmentId.create();
-        console.log("new: " + id.htmlId);
-        var html = '<li id="' + id.htmlId + '"></li>';
-        $(RouterFragment.routerId.cssId).append(html);
-
-        var parent = this.current;
-        this.current = new fragmentClass();
-        this.current.init($(id.cssId), parent);
-        this.currentId = id;
-        this.current.onStart();
     }
 }
